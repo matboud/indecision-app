c7b9a7bd982a5f7104ba1e0b8dfbf524a4686555
Matboud <contact@matboud.com>
2019-05-31 16:13:43 +0000

    playing around with objects

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,65 @@
 'use strict';
 
-console.log('hello');
+console.log("hello");
 
+var app = {
+    title: 'title',
+    subtitle: 'subtitle'
+};
 var template = React.createElement(
-  'p',
-  null,
-  'hello react'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        app.title
+    ),
+    React.createElement(
+        'p',
+        null,
+        app.subtitle
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'item'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'item'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'item'
+        )
+    )
 );
-var appRoot = document.getElementById('app');
+
+var name = 'amine';
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        name
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: 24'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: kenitra'
+    )
+);
+var appRoot = document.getElementById("app");
 
 ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,7 +1,29 @@
-console.log('hello');
+console.log("hello");
 
+var app = {
+    title: 'title',
+    subtitle: 'subtitle'
+};
+var template = (
+  <div>
+    <h1>{app.title}</h1>
+    <p>{app.subtitle}</p>
+    <ol>
+        <li>item</li>
+        <li>item</li>
+        <li>item</li>
+    </ol>
+  </div>
+);
 
-var template = <p>hello react</p>;
-var appRoot = document.getElementById('app');
+let name = 'amine';
+var templateTwo = (
+    <div>
+        <h1>{name}</h1>
+        <p>Age: 24</p>
+        <p>Location: kenitra</p>
+    </div>
+);
+var appRoot = document.getElementById("app");
 
-ReactDOM.render(template, appRoot)
+ReactDOM.render(template, appRoot);
