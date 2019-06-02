9f48456c705c39538d1b5266bde984a2054ca3eb
Matboud <contact@matboud.com>
2019-06-02 04:46:17 +0000

    im seriusly having fun with the arrays

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,53 +8,88 @@ var app = {
   options: []
 };
 
-var onFormSubmit = function onFormSubmit() {};
+var onFormSubmit = function onFormSubmit(e) {
+  e.preventDefault();
 
-var template = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
-    null,
-    app.title
-  ),
-  app.subtitle && React.createElement(
-    'p',
-    null,
-    app.subtitle
-  ),
-  React.createElement(
-    'p',
-    null,
-    app.options.length > 0 ? 'Here are your options' : 'No options'
-  ),
-  React.createElement(
-    'ol',
+  var option = e.target.elements.option.value;
+
+  if (option) {
+    app.options.push(option);
+    e.target.elements.option.value = '';
+    console.log('options.length: ' + app.options.length);
+    rerender();
+  }
+};
+
+var resetOptions = function resetOptions() {
+  app.options = [];
+  rerender();
+};
+
+var rerender = function rerender() {
+  var template = React.createElement(
+    'div',
     null,
     React.createElement(
-      'li',
+      'h1',
       null,
-      'Item one'
+      app.title
+    ),
+    app.subtitle && React.createElement(
+      'p',
+      null,
+      app.subtitle
     ),
     React.createElement(
-      'li',
+      'p',
       null,
-      'Item two'
-    )
-  ),
-  React.createElement(
-    'form',
-    { onSubmit: onFormSubmit },
-    React.createElement('input', { type: 'text', name: 'option' }),
+      app.options.length > 0 ? 'Here are your options' : 'No options'
+    ),
+    React.createElement(
+      'p',
+      null,
+      app.options.length
+    ),
+    React.createElement(
+      'ol',
+      null,
+      React.createElement(
+        'li',
+        null,
+        'Item one'
+      ),
+      React.createElement(
+        'li',
+        null,
+        'Item two'
+      ),
+      app.options.map(function (singleOption, index) {
+        return React.createElement(
+          'li',
+          { key: index },
+          singleOption
+        );
+      })
+    ),
     React.createElement(
       'button',
-      null,
-      'Add Option'
+      { onClick: resetOptions },
+      'reset options'
+    ),
+    React.createElement(
+      'form',
+      { onSubmit: onFormSubmit },
+      React.createElement('input', { type: 'text', name: 'option' }),
+      React.createElement(
+        'button',
+        null,
+        'Add Option'
+      )
     )
-  )
-);
+  );
+  ReactDOM.render(template, appRoot);
+};
 
 var appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
 
-render();
+rerender();
--- src/app.js
+++ src/app.js
@@ -6,30 +6,52 @@ const app = {
   options: []
 };
 
-const onFormSubmit = () => {
-  
+const onFormSubmit = (e) => {
+  e.preventDefault();
+
+  const option = e.target.elements.option.value;
+
+  if(option) {
+    app.options.push(option);
+    e.target.elements.option.value = '';
+    console.log('options.length: '+app.options.length)
+    rerender();
+  }
 };
 
+const resetOptions = () => {
+  app.options = [];
+  rerender();
+}
 
+const rerender = () => {
   const template = (
     <div>
       <h1>{app.title}</h1>
       {app.subtitle && <p>{app.subtitle}</p>}
       <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+      <p>{app.options.length}</p>
       <ol>
         <li>Item one</li>
         <li>Item two</li>
+        {
+          app.options.map((singleOption, index) => {
+            return <li key={index}>{singleOption}</li>
+          })
+        }
       </ol>
+      <button onClick={resetOptions}>reset options</button>
       <form onSubmit={onFormSubmit}>
         <input type="text" name="option" />
         <button>Add Option</button>
       </form>
     </div>
   );
-
+  ReactDOM.render(template, appRoot);
+}
+  
 
 const appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
 
+rerender();
 
-render();
