189fde5dbec42de8e71ed58fd3996c1fec9c8a85
Matboud <contact@matboud.com>
2019-06-08 09:23:45 +0000

    init babel from webpack

--- src/app.js
+++ src/app.js
@@ -1,3 +1,6 @@
-import validator from 'validator';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-console.log(validator.isEmail('test@gmail.com'))
+const template = React.createElement('p', {}, 'testo');
+
+ReactDOM.render(template, document.getElementById('app'));
