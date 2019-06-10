d1603da01b3b5d7ded11eccd6feee8a040b0b2bf
Matboud <contact@matboud.com>
2019-06-10 16:19:21 +0100

    adding SASS and including it in WEBPACK

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
-
+import './styles/styles.scss';
 
 ReactDOM.render(<IndecisionApp  />, document.getElementById("app"));
