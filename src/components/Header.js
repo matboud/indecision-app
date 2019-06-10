0085fcdb5e8bfd37f2f689e20069744a332c1825
Matboud <contact@matboud.com>
2019-06-10 17:31:31 +0100

    running some tests & adding normalize.css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,9 @@
 import React from "react";
 
 const Header = props => (
-  <div>
-    <h1>{props.title}</h1>
-    {props.subTitle && <h2>{props.subTitle}</h2>}
+  <div className='header'>
+    <h1 className='header__title'>{props.title}</h1>
+    {props.subTitle && <h2 className= 'header__subtitle'>{props.subTitle}</h2>}
   </div>
 );
 
